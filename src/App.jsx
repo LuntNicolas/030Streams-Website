17a5df5693a752c615566346df5959b2096d9db4
LuntNicolas <107206933+LuntNicolas@users.noreply.github.com>
2025-05-16 13:28:11 +0200

    logo & images

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <main>
             <div>
-                <h1>030 Streams</h1>
+                <h1 className="text-3xl">030 Streams</h1>
             </div>
         </main>
     )
