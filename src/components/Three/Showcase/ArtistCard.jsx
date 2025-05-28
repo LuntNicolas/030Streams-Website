5af7a080645d223e830ca3449131f79e25e4754f
LuntNicolas <107206933+LuntNicolas@users.noreply.github.com>
2025-05-28 23:09:07 +0200

    Init FIle

--- src/components/Three/Showcase/ArtistCard.jsx
+++ src/components/Three/Showcase/ArtistCard.jsx
@@ -5,15 +5,14 @@ import ArtistMesh from "./ArtistMesh.jsx"
 const ArtistCard = ({index, scrollProgress}) => {
     const DIST = 10
     const targetZ = useMemo(() => -index * (DIST), [index])
-    console.log(index)
 
     const artist = artists[index]
-
     return (
         <group>
             {artist.items.map((item, index) => {
                 const x = index % 2 === 0 ? -4 : 4
-                return <ArtistMesh key={index} item={item} position={[x, 0, targetZ]}/>
+                const zOffset = -index * 1.5
+                return <ArtistMesh key={index} item={item} position={[x, 0, targetZ + zOffset]}/>
             })}
         </group>
 
