f5457b0db74b53a48c5a278fe831091e08243ee8
LuntNicolas <107206933+LuntNicolas@users.noreply.github.com>
2025-05-29 10:33:49 +0200

    Git Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import glsl from 'vite-plugin-glsl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-      react(),
-    glsl()
-  ],
+    plugins: [
+        tailwindcss(),
+        react(),
+        glsl()
+    ],
+    base: '/030Streams-Website/'
 })
